b4e4c8d4af0b22072899ecc440ae396c0b46a0b4
Cameron <c@cameronfrost.com>
2020-12-28 11:34:47 -0600

    Session storage

--- shoppingCart.js
+++ shoppingCart.js
@@ -8,3 +8,26 @@ const cartQuantity = document.querySelector("[data-cart-quantity]");
 const cartTotal = document.querySelector("[data-cart-total]");
 const cart = document.querySelector("[data-cart]");
 const SESSION_STORAGE_KEY = "SHOPPING_CART-cart";
+
+shoppingCart = loadCart();
+renderCart();
+
+function saveCart() {
+  sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(shoppingCart));
+}
+
+function loadCart() {
+  const cart = sessionStorage.getItem(SESSION_STORAGE_KEY);
+  return JSON.parse(cart) || [];
+}
+
+export function addToCart(id) {
+  const existingItem = shoppingCart.find(entry => entry.id === id);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    shoppingCart.push({ id: id, quantity: 1 });
+  }
+  renderCart();
+  saveCart();
+}
